e1005958c8e4bb15217e9f4ef2254c06f8395938
Jeff Rios <jeff_rios2002@outlook.com>
2024-02-06 12:14:11 -0500

    Se ha cambiado la forma de validar el ingreso de numeros y numeros repetidos por los jugadores Se ha agregado la función de centrar para colocar el texto en el centro Se ha agregado las variables para cambiar el tamaño de la ventana

--- TresEnRaya/Program.cs
+++ TresEnRaya/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Remoting.Messaging;
 
 
 namespace TresEnRaya
@@ -7,11 +8,19 @@ namespace TresEnRaya
     {
         static void Main(string[] args)
         {
+            
+            //Ajustes de ventana de consola
+            const int anchoVentana = 50;
+            const int altoVentana = 25;
+
+            Console.WindowHeight = altoVentana;
+            Console.WindowWidth = anchoVentana;
 
             int jugador = 2;
+            int jugadorActual = 2;
             int ingreso = 0;
             bool validarIngreso = true;
-      
+            
             do
             {
 
@@ -24,9 +33,10 @@ namespace TresEnRaya
                 {
                     jugador = 2;
                     logicaJuego(jugador, ingreso);
-                }
+                }  
+                
                 dibujarTablero();
-
+            
                 #region
                 char[] cadaSigno = { 'X', 'O' };
                 foreach (char signo in cadaSigno)
@@ -40,7 +50,8 @@ namespace TresEnRaya
                 #region
                 do
                 {
-                    Console.WriteLine("\nJugador {0}, por favor elija un casillero: ", jugador);
+                    Console.WriteLine("\n  Jugador {0}, por favor elija un casillero: ", jugador);
+                    Console.Write("  ");
                     try
                     {
                         ingreso = Convert.ToInt32(Console.ReadLine());
@@ -48,32 +59,52 @@ namespace TresEnRaya
                     catch (Exception)
                     {
 
-                        Console.WriteLine("Solo debe ingresar números");
+                        Console.WriteLine("  Solo debe ingresar números");
                     }
-                    if ((ingreso == 1) && tableroJuego[0, 0] == '1')
-                        validarIngreso = true;
-                    else if ((ingreso == 2) && tableroJuego[0, 1] == '2')
-                        validarIngreso = true;
-                    else if ((ingreso == 3) && tableroJuego[0, 2] == '3')
-                        validarIngreso = true;
-                    else if ((ingreso == 4) && tableroJuego[1, 0] == '4')
-                        validarIngreso = true;
-                    else if ((ingreso == 5) && tableroJuego[1, 1] == '5')
-                        validarIngreso = true;
-                    else if ((ingreso == 6) && tableroJuego[1, 2] == '6')
-                        validarIngreso = true;
-                    else if ((ingreso == 7) && tableroJuego[2, 0] == '7')
-                        validarIngreso = true;
-                    else if ((ingreso == 8) && tableroJuego[2, 1] == '8')
-                        validarIngreso = true;
-                    else if ((ingreso == 9) && tableroJuego[2, 2] == '9')
-                        validarIngreso = true;
+                    /*
+                    if      ((ingreso == 1) && tableroJuego[0, 0] == '1')   validarIngreso = true;
+                    else if ((ingreso == 2) && tableroJuego[0, 1] == '2')   validarIngreso = true;
+                    else if ((ingreso == 3) && tableroJuego[0, 2] == '3')   validarIngreso = true;
+                    else if ((ingreso == 4) && tableroJuego[1, 0] == '4')   validarIngreso = true;
+                    else if ((ingreso == 5) && tableroJuego[1, 1] == '5')   validarIngreso = true;
+                    else if ((ingreso == 6) && tableroJuego[1, 2] == '6')   validarIngreso = true;
+                    else if ((ingreso == 7) && tableroJuego[2, 0] == '7')   validarIngreso = true;
+                    else if ((ingreso == 8) && tableroJuego[2, 1] == '8')   validarIngreso = true;
+                    else if ((ingreso == 9) && tableroJuego[2, 2] == '9')   validarIngreso = true;
                     else
                     {
                         Console.WriteLine("Por Favor ingrese otro número");
                         validarIngreso = false;
                     }
+                    */
 
+                    for (int x = 0; x < 10; x++)
+                    {   
+                        for (int i = 0; i < tableroJuego.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < tableroJuego.GetLength(0); j++)
+                            {
+                                if(ingreso == Convert.ToInt32(x.ToString()) && tableroJuego[i,j] == Convert.ToChar(x.ToString()))
+                                {
+                                    validarIngreso = true;
+                                    Console.WriteLine(Convert.ToInt32(x.ToString()) + " == " + tableroJuego[i, j]);
+                                    break;
+                                }
+                                else
+                                {
+                                    validarIngreso = false;
+                                }    
+                            }
+                            if (validarIngreso) break;
+                        }
+                        if (validarIngreso) break;
+                    }
+
+                    if (!validarIngreso)
+                    {
+                        dibujarTablero();
+                        Console.WriteLine("\n  Ese numero ya esta ocupado por el jugador {0}\n  Ingresa otro valor", jugadorActual = (jugador == 1) ? 2 : 1);
+                    }
                 }
                 while (!validarIngreso);
                 #endregion
@@ -86,21 +117,46 @@ namespace TresEnRaya
 
         static char[,] tableroJuego = { { '1', '2', '3' }, { '4', '5', '6' }, { '7', '8', '9' } };
 
+        public static void centrarTexto(string texto)
+        {
+            int anchoVentana = Console.WindowWidth;
+            int longitudTexto = texto.Length;
+
+            int espaciosAlInicio = (anchoVentana - longitudTexto) / 2;
+
+            Console.Write(new string(' ', espaciosAlInicio) + texto);
+        }
+
+
         //Creación de tablero
         public static void dibujarTablero()
         {
+
+            //Para centrar tablero
+            int ventana = Console.WindowWidth;
+            int espacioMargen = ("  |  ").Length;
+            int centroReferencia = ("._________________.").Length;
+            int espacioTotal = (ventana - (centroReferencia + espacioMargen)) / 2;
+
             Console.Clear();
-            Console.WriteLine("._________________.");
+            Console.WriteLine("");
+            centrarTexto("Juego 3 en Raya\n\n");
+            centrarTexto("Ronda: " + 1 + "          Ganador Anterior: " + "Jeff\n\n");
+
+
+            centrarTexto("._________________.\n");
             for (int j = 0; j < 3; j++)
             {
-                Console.WriteLine("|     |     |     |");
+                centrarTexto("|     |     |     |\n");
+                Console.Write(new string(' ', espacioTotal));
                 for (int i = 0; i < 3; i++)
                 {
-                    Console.Write("| ");
-                    Console.Write(" {0}  ", tableroJuego[j, i]);
-                }
-                Console.Write("|\n");
-                Console.WriteLine("|_____|_____|_____|");
+                    Console.Write("  |  ");
+                    Console.Write(""+ tableroJuego[j, i].ToString()+ "");
+                     
+                } 
+                Console.Write("  |\n");
+                centrarTexto("|_____|_____|_____|\n");
             }
         }
 
@@ -150,6 +206,5 @@ namespace TresEnRaya
                 default: break;
             }
         }
-
     }
 }
